ec488f8a4c74673dba5f34d7b69233d12fd1c2ba
amo-yotakenoko <amo-yotakenoko@gmail.com>
2025-01-12 21:28:53 +0900

    qr_reader整理

--- flask_inventory/static/qr_reader.js
+++ flask_inventory/static/qr_reader.js
@@ -5,47 +5,47 @@ const codes = [];  //呼んだURLをここにためる
 //これの中で書かないとHTML要素が生成される前に実行されてgetEleentが死ぬ
 window.onload = (e) => {
 
-	//要素を取得
-	let video  = document.createElement("video");
-	let canvas = document.getElementById("canvas");
-	let ctx    = canvas.getContext("2d");
-	let msg    = document.getElementById("msg");
+    //要素を取得
+    let video  = document.createElement("video");
+    let canvas = document.getElementById("canvas");
+    let ctx    = canvas.getContext("2d");
+    let msg    = document.getElementById("msg");
 
-	//カメラの準備が出来たらスタート
-	const userMedia = {video: {facingMode: "environment"}};
-	navigator.mediaDevices.getUserMedia(userMedia).then((stream)=>{
-		video.srcObject = stream;
-		video.setAttribute("playsinline", true);
-		video.play();
-		startTick();
-	});
+    //カメラの準備が出来たらスタート
+    const userMedia = {video: {facingMode: "environment"}};
+    navigator.mediaDevices.getUserMedia(userMedia).then((stream)=>{
+        video.srcObject = stream;
+        video.setAttribute("playsinline", true);
+        video.play();
+        startTick();
+    });
 
     function startTick() {
-		/* msg.innerText = "Loading video..."; */
-		if(video.readyState === video.HAVE_ENOUGH_DATA){
-			canvas.height = video.videoHeight;
-			canvas.width = video.videoWidth;
-			ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-			// let img = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        /* msg.innerText = "Loading video..."; */
+        if(video.readyState === video.HAVE_ENOUGH_DATA){
+            canvas.height = video.videoHeight;
+            canvas.width = video.videoWidth;
+            ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+            // let img = ctx.getImageData(0, 0, canvas.width, canvas.height);
 
-			while (true) {
+            while (true) {
 
-			 //canvasからコピーしてjsqrに渡す
+             //canvasからコピーしてjsqrに渡す
             let img = ctx.getImageData(0, 0, canvas.width, canvas.height);
              code = jsQR(img.data, img.width, img.height, { inversionAttempts: "dontInvert" })
              
-			 console.log(code)
-			 
-			 //無限ループだがコードを読める場所がなくなったらそのフレームを終了
+             console.log(code)
+             
+             //無限ループだがコードを読める場所がなくなったらそのフレームを終了
                 if (!code) break;
-			 // if(code){
-			 //前呼んだ奴は聞かないように
-			 if (!codes.includes(code.data)) {
-				 codes.push(code.data);
-				 get_data(code.data)
-}
+             // if(code){
+             //前呼んだ奴は聞かないように
+             if (!codes.includes(code.data)) {
+                 codes.push(code.data);
+                 get_data(code.data)
+            }
             //  codes.push(code.data)
-			//前QRコードを詠んだ場所を読まないように塗りつぶし
+            //前QRコードを詠んだ場所を読まないように塗りつぶし
                     drawRect(code.location);
                     //  }
                 }
@@ -53,50 +53,36 @@ window.onload = (e) => {
           
             
 
-		}
-		//10ms待ってこの関数を読んでつぎフレームへ
-		setTimeout(startTick, 10);
-	}
+        }
+        //10ms待ってこの関数を読んでつぎフレームへ
+        setTimeout(startTick, 10);
+    }
 
-	//リセットボタン
+    //リセットボタン
       document.getElementById('resetButton').addEventListener('click', () => {
          
-		  console.log("リセット")
-		 codes.length = 0; 
-		   msg ="";
+          console.log("リセット")
+         codes.length = 0; 
+           msg ="";
            const table = document.getElementById('data-table').getElementsByTagName('tbody')[0];
 
-table.innerHTML = '';
+            table.innerHTML = '';
         });
 
 
 //前QRコードを詠んだ場所を読まないように塗りつぶし
-	function drawRect(location){
+    function drawRect(location){
         ctx.beginPath();
         ctx.moveTo(location.topLeftCorner.x, location.topLeftCorner.y);
         ctx.lineTo(  location.topRightCorner.x,   location.topRightCorner.y);
         ctx.lineTo(  location.bottomRightCorner.x,   location.bottomRightCorner.y);
         ctx.lineTo(location.bottomLeftCorner.x, location.bottomLeftCorner.y);
         ctx.lineTo(location.bottomLeftCorner.x, location.bottomLeftCorner.y);
-  		ctx.fillStyle = "red";
-  		ctx.fill();
-        // ctx.stroke();
-		// drawLine(location.topLeftCorner,     location.topRightCorner);
-		// drawLine(location.topRightCorner,    location.bottomRightCorner);
-		// drawLine(location.bottomRightCorner, location.bottomLeftCorner);
-		// drawLine(location.bottomLeftCorner,  location.topLeftCorner);
-	}
-//没
-	// function drawLine(begin, end){
-	// 	ctx.lineWidth = 4;
-	// 	ctx.strokeStyle = "#FF3B58";
-	// 	ctx.beginPath();
-	// 	ctx.moveTo(begin.x, begin.y);
-	// 	ctx.lineTo(end.x, end.y);
-    //     ctx.fillStyle = "blue";
-    //     // ctx.stroke();
-    //     // ctx.fill();
-	// }
+          ctx.fillStyle = "red";
+          ctx.fill();
+
+    }
+
 
 
 // fetchでURLを問い合わせる
@@ -143,6 +129,4 @@ function addRowToTable(data) {
     cellPurchaseDate.textContent = data.purchase_date || "N/A";
 }
 
-
-	
 };
